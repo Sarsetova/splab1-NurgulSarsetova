288cd394d068d21e77a4e20df6d0beabf647b807
Sarsetova <32267522+Sarsetova@users.noreply.github.com>
2017-09-25 20:17:28 +0600

    Create cos.c

---
+++ cos.c
@@ -0,0 +1,12 @@
+//Sarsetova Nurgul
+//3EN04B
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+int main(int argc, char*argv[]){
+  double angle=atof(argv[1]);
+  printf("sin(%f)\n",sin(angle));
+  printf("cos(%f)\n", cos(angle));
+  return 0;
+}
